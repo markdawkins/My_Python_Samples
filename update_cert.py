1d3ebcc7b038013df8e37e6cc6e6f2368bfb0e2d
markdawkins <44737733+markdawkins@users.noreply.github.com>
2022-04-27 20:59:40 -0400

    Create update_cert.py

---
+++ update_cert.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python
+from getpass import getpass
+#import csv
+#from jinja2 import Template
+from netmiko import ConnectHandler
+#from netmiko.ssh_exception import NetMikoTimeoutException
+#from netmiko.ssh_exception import SSHException
+#from netmiko.ssh_exception import AuthenticationException
+#from netmiko import SCPConn
+from datetime import datetime
+
+username = input("Enter username:")
+password = getpass("Enter password: ")
+host = input ("Enter Host ip or dns name: ")
+
+
+# Device connect parameters
+device = {
+    'device_type': 'cisco_ios',
+    'host': host,
+    'username': username,
+    'password': password,
+    'port': 22,
+}
+
+net_connect = ConnectHandler(**device)
+
+
+print("enable")
+output1 = net_connect.send_command_timing("enable")
+print(output1)
+print("config t")
+output2 = net_connect.send_command_timing("configure terminal")
+print(output2)
+print(" no crypto pki trustpoing DNAC-CA")
+output3 = net_connect.send_command_timing("no crypto pki trustpoint DNAC-CA")
+print(output3)
+print("yes")
+output4 = net_connect.send_command_timing("yes")
+print(output4)
+output5 = net_connect.send_command_timing("end")
+print("end")
+print(output5)
+print (" ")
+print ( " DNAC cert has been removed ")
+
+
+
+#add new cert here ########
+config_commands6 = ['crypto pki trustpoint DNAC-CA' ,  
+                    'enrollment mode ra ',
+                    'enrollment terminal' ,
+                    'usage ssl-client' ,
+                    'revocation-check none',
+                    'crypto pki certificate chain DNAC-CA' ,
+                    'certificate ca xxxxx xxxx xxxxx xxxxxxxxx xxxxxxxxxx' ,
+                    'OUPUT OMITTED  here real cert goes here '
+                    '3C9D16',
+                    'quit',
+                    'end',
+                    'wr mem'
+                    
+                ]
+output6 = net_connect.send_config_set(config_commands6, exit_config_mode=False)
+print(output6)
