906bdf0f26382d3091cbe2a052df3b69eb75d76b
markdawkins <44737733+markdawkins@users.noreply.github.com>
2022-06-03 12:27:22 -0400

    Update switch_report.py
    Modifieid show ver command.
     Added better formatting so there is a space between the output of the comamnds.

--- switch_report.py
+++ switch_report.py
@@ -7,9 +7,9 @@ logdate = datetime.now().strftime("%b")
 
 host = input("Enter Host/IP: ")
 commands = ['term length 0', 'show int status | inc connected' , 'show int status | count  connected' , 'show mac address-table  | inc DYNAMIC'
-,'show mac address-table  | count DYNAMIC' , 'show cdp nei | inc AP|ap' , 'show ip int brie | exc unassigned'  , 'show run' , 'show log | inc ' + logdate ]
+,'show mac address-table  | count DYNAMIC' , 'show cdp nei | inc AP|ap' , 'show ip int brie | exc unassigned'  ,'show ver | inc Version' , 'show run' , 'show log | inc ' + logdate ]
 
-outputFileName = host + subname +'_output.txt'
+outputFileName = host +'_'+ subname +'_output.txt'
 
 user_name = input("Enter username:")
 password = getpass("Enter password: ")
@@ -22,10 +22,13 @@ iosv_l2 = {
 }
 
 net_connect = ConnectHandler(**iosv_l2)
-with open(outputFileName, 'w') as f:
+with open("/Users/014mdawkins/Documents/SWITCH_REPORTS/"+  outputFileName, 'w') as f:
     for command in commands:
                 net_connect.send_command(command)
                 output = net_connect.send_command(command)
+                f.write("\n")
+                f.write("\n")
+                f.write("\n")
                 f.write(output)
              
     
