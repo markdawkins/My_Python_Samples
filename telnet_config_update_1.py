8689ab104ee06e261e1db64b0f84e36129fc9175
markdawkins <44737733+markdawkins@users.noreply.github.com>
2022-10-01 22:42:56 -0400

    Update telnet_config_update_1.py

--- telnet_config_update_1.py
+++ telnet_config_update_1.py
@@ -14,6 +14,23 @@ HOST = input("Enter host name")
 user = input("Enter your telnet username: ")
 password = getpass.getpass()
 
+
+
+# USE AS REFERENCE TO PICK HOSTS FROM LIST ##
+#for line in f:
+#    host = line 
+#    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+#    rep = os.system( 'ping ' + host)
+#    if rep == 0: 
+#	    output=('Server is up: {:%Y-%b-%d %H:%M:%S}'.format(datetime.datetime.now()))
+	    
+ #   else:
+ #	    output=('Server is down: {:%Y-%b-%d %H:%M:%S}'.format(datetime.datetime.now()))
+
+
+
+
+
 tn = telnetlib.Telnet(HOST)
 
 tn.read_until("Username: ")
