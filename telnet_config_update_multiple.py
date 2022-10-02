7291c05295a87c39e4e8107aa51c850175caae2f
mark <mark@pop-os.localdomain>
2022-10-02 12:40:15 -0400

    updated to telnet config script for multiple nodes

--- telnet_config_update_multiple.py
+++ telnet_config_update_multiple.py
@@ -4,46 +4,37 @@ import getpass
 import sys
 import telnetlib
 
+LIST = input("Enter List Name:")
+listname = "./LISTS/%s.txt"  % LIST
+f = open (listname)
 
-#LIST = input("Enter List Name:")
-#listname = "./LISTS/%s.txt"  % LIST
-#f = open (listname)
 
-
-HOST = input("Enter host name: ")
+#HOST = input("Enter host name: ")
 user = input("Enter your telnet username: ")
 password = getpass.getpass()
 
 
 # USE AS REFERENCE TO PICK HOSTS FROM LIST ##
-#for line in f:
-#    host = line 
-#    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-#    rep = os.system( 'ping ' + host)
-#    if rep == 0: 
-#	    output=('Server is up: {:%Y-%b-%d %H:%M:%S}'.format(datetime.datetime.now()))
-	    
- #   else:
- #	    output=('Server is down: {:%Y-%b-%d %H:%M:%S}'.format(datetime.datetime.now()))
+for line in f:
+    HOST = line 
+    #port = 23
+    #timeout = 100 
+    tn = telnetlib.Telnet(HOST)
+    tn.read_until(b"Username: ")
+    tn.write(user.encode('ascii') + b"\n")
+    if password:
+        tn.read_until(b"Password: ")
+        tn.write(password.encode('ascii') + b"\n")
+
+    tn.write(b"conf t\n")
+
+    tn.write(b"hostname TEST\n")
+    tn.write(b"logging host 10.10.10.10\n")
+
+    tn.write(b"end\n")
+    tn.write(b"exit\n")
+    tn.write(b"wr mem\n")
+
+    print(tn.read_all().decode('ascii'))
 
 
-tn = telnetlib.Telnet(HOST)
-
-tn.read_until(b"Username: ")
-tn.write(user.encode('ascii') + b"\n")
-if password:
-    tn.read_until(b"Password: ")
-    tn.write(password.encode('ascii') + b"\n")
-
-tn.write(b"conf t\n")
-
-tn.write(b"hostname R1\n")
-tn.write(b"no logging host 10.10.10.10\n")
-
-tn.write(b"end\n")
-tn.write(b"exit\n")
-tn.write(b"wr mem\n")
-
-print(tn.read_all().decode('ascii'))
-
-###TESTED AND VERIFIED TO WORK ###
