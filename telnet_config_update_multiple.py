8075461875cd7e21c272be75714eca53602e45de
mark <mark@pop-os.localdomain>
2022-10-02 12:18:18 -0400

    created new telnet scripts

--- telnet_config_update_multiple.py
+++ telnet_config_update_multiple.py
@@ -4,9 +4,28 @@ import getpass
 import sys
 import telnetlib
 
+
+#LIST = input("Enter List Name:")
+#listname = "./LISTS/%s.txt"  % LIST
+#f = open (listname)
+
+
 HOST = input("Enter host name: ")
 user = input("Enter your telnet username: ")
 password = getpass.getpass()
+
+
+# USE AS REFERENCE TO PICK HOSTS FROM LIST ##
+#for line in f:
+#    host = line 
+#    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+#    rep = os.system( 'ping ' + host)
+#    if rep == 0: 
+#	    output=('Server is up: {:%Y-%b-%d %H:%M:%S}'.format(datetime.datetime.now()))
+	    
+ #   else:
+ #	    output=('Server is down: {:%Y-%b-%d %H:%M:%S}'.format(datetime.datetime.now()))
+
 
 tn = telnetlib.Telnet(HOST)
 
@@ -26,4 +45,5 @@ tn.write(b"exit\n")
 tn.write(b"wr mem\n")
 
 print(tn.read_all().decode('ascii'))
-### TESTED AND VERIFIED TO WORK ###
+
+###TESTED AND VERIFIED TO WORK ###
