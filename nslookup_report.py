cc4888ef0df2cc5a38188a89c7ebcd467bc1b2d1
markdawkins <44737733+markdawkins@users.noreply.github.com>
2025-02-19 19:05:43 -0500

    Update nslookup_report.py

--- nslookup_report.py
+++ nslookup_report.py
@@ -1,5 +1,7 @@
+
 import socket
 import csv
+import os
 
 # Function to perform nslookup and return IP address
 def nslookup(hostname):
@@ -10,8 +12,10 @@ def nslookup(hostname):
 
 # Main function to handle user input and generate the report
 def main():
-    # Get the file containing hostnames from the user
-    file_path = input("Enter the path to the file containing hostnames: ")
+    # Set the file path to the predefined directory
+    directory = "C:\\Users\\mark\\Desktop\\Code\\Py_Scripts\\Prod\\lists"
+    file_name = input("Enter the name of the file containing hostnames: ")
+    file_path = os.path.join(directory, file_name)
     
     # Read hostnames from file
     try:
